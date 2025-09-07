0f65e15363bef1ae1d4e06c6d73f795e6b74afa4
Siddhesh Zujam <siddheshzujam111@gmail.com>
2025-09-07 15:49:04 +0530

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Employee_Management_System-EMS/",
+  base: "/Employee-Management-System-EMS/",
   build: {
     rollupOptions: {
       output: {
