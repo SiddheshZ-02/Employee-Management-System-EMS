07226a5f342a8634e2ad39b9d2a22d5ebdcb2ff3
Siddhesh Zujam <siddheshzujam111@gmail.com>
2025-09-07 16:06:56 +0530

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   BrowserRouter,
-  HashRouter as Routes,
+  Routes,
   Route,
   Navigate,
 } from "react-router-dom";
